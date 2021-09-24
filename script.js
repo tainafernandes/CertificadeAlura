eab6d5b33df4cd4dcccbe22f2b7439878550a832
tainafernandes <87373947+tainafernandes@users.noreply.github.com>
2021-09-24 13:50:07 -0300

    arquivos do projeto Portfolio Alura.

---
+++ script.js
@@ -0,0 +1,8 @@
+function mudaModo() {
+  document.body.classList.toggle("dark");
+}
+
+/* DESAFIO AULA 9
+Terminar todo o design que está no figma (disponível abaixo)
+Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+*/
